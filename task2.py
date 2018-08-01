2c3f7ae35336a9ea16cc58fe97b774fdc80c2cad
amuguna6 <37804982+amuguna6@users.noreply.github.com>
2018-08-01 17:48:08 +0300

    Update task2.py
    Completed Task 2

--- task2.py
+++ task2.py
@@ -4,3 +4,6 @@ words = [word.rstrip('\n') for word in open('words.txt')]
 randomPhrase = " ".join([words[random.randrange(0, len(words))] for i in range(4)])
 
 randomPhrase
+ReverseWord = randomPhrase[::-1]
+
+print (ReverseWord)

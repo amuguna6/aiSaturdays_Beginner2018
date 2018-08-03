9b73712645d6c191bf65aeb4599729709443cb1c
amuguna6 <37804982+amuguna6@users.noreply.github.com>
2018-08-03 14:19:50 +0300

    Update task1.py
    Completed Task 1

--- task1.py
+++ task1.py
@@ -7,3 +7,14 @@ while (count < 11):
     count += 1
     
 ourList
+#print(ourList) 
+    belowFive=[i for i in ourList if i<5] #sort all integers less than Five for ourList Array
+
+$ cd aiSaturdays_Beginner2018
+$ git pull origin master
+$ git checkout -b amuguna6@gmail.com master
+$ git branch
+* amuguna6@gmail.com
+  master
+    $ git push origin -u amuguna6@gmail.com
+    print(belowFive)

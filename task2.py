259118a9be90c57acf7c6c79152213e4bd0e249c
amuguna6 <37804982+amuguna6@users.noreply.github.com>
2018-08-03 14:46:14 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -4,3 +4,7 @@ words = [word.rstrip('\n') for word in open('words.txt')]
 randomPhrase = " ".join([words[random.randrange(0, len(words))] for i in range(4)])
 
 randomPhrase
+ReverseWord = randomPhrase[::-1]
+$ cd aiSaturdays_Beginner2018
+$ git pull origin master
+print (ReverseWord)
